5aae4ce921553f1bc59b499f4a5c8f2dd1464501
Kevin <NateraKevin1@gmail.com>
2022-04-28 15:59:30 -0400

    created shorturl generator

---
+++ URLShortener/app/models/shortened_url.rb
@@ -0,0 +1,47 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id         :bigint           not null, primary key
+#  short_url  :string           not null
+#  long_url   :string           not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class ShortenedURL < ApplicationRecord
+
+    belongs_to :user,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :User
+
+    def self.random_code 
+        url = SecureRandom::urlsafe_base64(22)
+        
+        while self.exists?(url)
+            url = SecureRandom::urlsafe_base64(22)
+        end
+
+        url
+    end
+
+    def self.generate_shorturl(user, long_url)
+        short_url = self.random_code
+       
+        ShortenedURL.create!(short_url: short_url[0..5], long_url: long_url, user_id: user.id)
+    end
+end
+
+# require 'base64'
+# $global_hash = Hash.new
+# def encode(longUrl)
+#     shortUrl = Base64.encode64(longUrl)[0..5]
+#     $global_hash[shortUrl] = longUrl
+#     shortUrl
+# end
+# def decode(shortUrl)
+#   $global_hash[shortUrl]
+# end
+# p decode(encode("https://leetcode.com/problems/design-tinyurl"))
+# p decode(encode("https://www.google.com"))
